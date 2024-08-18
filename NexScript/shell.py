1725e541fd20624a28500820c31a7b0eaf441105
memecoder <memecoder17@gmail.com>
2024-08-18 11:22:44 +0700

    shell.py

--- NexScript/shell.py
+++ NexScript/shell.py
@@ -1,12 +1,19 @@
 import NexScript
 while True:
     text = input('>>> ')
+    if text.strip() == "": continue
+    result, error = NexScript.run('<stdin>', text)
     if '$' in text:
         lines = text.splitlines()
         filtered_lines = [line for line in lines if '$' not in line]
         text = '\n'.join(filtered_lines)
-    if not text.split(): pass
-    else: 
-        result, error = NexScript.run('<stdin>', text)
-        if error: print(error.as_string())
-        else: print(result)
+    if error:
+        if hasattr(error, 'as_string'):
+            print(error.as_string())
+        else:
+            print(error)
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
