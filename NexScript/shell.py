05062c802fa815e93533cedc80b5bcd7414976ba
memecoder <memecoder17@gmail.com>
2024-08-28 22:05:52 +0700

    Add files via upload

---
+++ NexScript/shell.py
@@ -0,0 +1,40 @@
+import NexScript
+print('NexScript (v1.6.2, Aug 28 2024, 11:52:23)\nTIP: Use command "EXIT" to exit.')
+def clear_code(code):
+    cleaned_lines = []
+    inside_string = False
+    for line in code.splitlines():
+        new_line = ""
+        for char in line:
+            if char == '"' and not inside_string:
+                inside_string = True
+            elif char == '"' and inside_string:
+                inside_string = False
+            if char == '$' and not inside_string:
+                break 
+            new_line += char
+        if new_line.strip():
+            cleaned_lines.append(new_line)
+    return "\n".join(cleaned_lines)
+try:
+    while True:
+        text = input('>>> ')
+        text = clear_code(text)
+        if text.strip() == "": continue
+        if text == "EXIT": break
+        result, error = NexScript.run('<stdin>', text)
+        if error:
+            if hasattr(error, 'as_string'):
+                print(error.as_string())
+            else:
+                print(error)
+        elif result:
+            if len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
+except KeyboardInterrupt:
+    print('TIP: Use command "EXIT" to exit.')
+except Exception as e:
+    print(e)
+
