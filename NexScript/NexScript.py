ab08b7a27915c58e20a8da80408c03af82ede58e
memecoder <memecoder17@gmail.com>
2024-09-13 23:21:47 +0700

    Update NexScript.py

--- NexScript/NexScript.py
+++ NexScript/NexScript.py
@@ -1,4 +1,4 @@
-# NexScript (v1.7.0, Aug 28 2024, 11:52:23)
+# NexScript (v1.7.0, September 13 2024, 11:52:23)
 from strings_error import * # type: ignore
 import subprocess
 import string as string_ascii
